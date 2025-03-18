42db7931b8cc15a0df0ab25ddac63d24d46b8a2f
MykyBalb <mykaelbalbino06@gmail.com>
2025-03-17 21:56:47 -0300

    exercicio 2 atualizado

--- appCicloVidaMobile2/App.js
+++ appCicloVidaMobile2/App.js
@@ -1,59 +1,166 @@
 import React, { Component } from 'react';
-import { Text, View, Button, StyleSheet } from 'react-native';
+import { View, Text, TextInput, Button, Alert, StyleSheet, Platform, BackHandler } from 'react-native';
 
-class MeuComponente extends Component {
+class LoginScreen extends Component {
   constructor(props) {
     super(props);
-    this.state = { mensagem: 'Olá!', contador: 0, exibirComponente: true };
-    console.log('constructor: Componente montado!');
+    this.state = { username: '', password: '' };
+    console.log('LoginScreen: constructor');
   }
 
   componentDidMount() {
-    console.log('componentDidMount: Componente montado!');
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    console.log('componentDidUpdate: Componente atualizado:', prevState, this.state);
+    console.log('LoginScreen: componentDidMount');
   }
 
   componentWillUnmount() {
-    console.log('componentWillUnmount: Componente desmontado!');
-  }
-
-  alterarMensagem = () => {
-    this.setState({ mensagem: 'Nova mensagem!' });
-  };
-
-  incrementarContador = () => {
-    this.setState({ contador: this.state.contador + 1 });
-  };
-
-  exibirOuOcultarComponente = () => {
-    this.setState({ exibirComponente: !this.state.exibirComponente });
-  };
-
-  shouldComponentUpdate(nextProps, nextState) {
-    if (this.state.contador !== nextState.contador) {
-      console.log('shouldComponentUpdate: Contador mudou, renderizando');
-      return true;
-    }
-    console.log('shouldComponentUpdate: Contador não mudou, ignorando renderização');
-    return false;
+    console.log('LoginScreen: componentWillUnmount');
   }
 
   render() {
-    if (!this.state.exibirComponente) {
-      console.log('render: para não renderizar se exibirComponente.');
-      return null;
-    }
-    console.log('render: renderiza Componente.');
+    console.log('LoginScreen: render');
     return (
       <View style={styles.container}>
-        <Text style={styles.text}>{this.state.mensagem}</Text>
-        <Text style={styles.text}>Contador: {this.state.contador}</Text>
-        <Button title="Alterar Mensagem" onPress={this.alterarMensagem} color="#007bff" />
-        <Button title="Incrementar Contador" onPress={this.incrementarContador} color="#007bff" />
-        <Button title="Exibir/Ocultar Componente" onPress={this.exibirOuOcultarComponente} color="#007bff" />
+        <Text style={styles.title}>Login</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Usuário"
+          onChangeText={(username) => this.setState({ username })}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Senha"
+          secureTextEntry
+          onChangeText={(password) => this.setState({ password })}
+        />
+        <Button
+          title="Entrar"
+          onPress={() => this.props.onLogin(this.state.username, this.state.password)}
+        />
+      </View>
+    );
+  }
+}
+
+class CadastroScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nome: '',
+      endereco: '',
+      cidade: '',
+      cep: '',
+      telefone: '',
+    };
+    console.log('CadastroScreen: constructor');
+  }
+
+  componentDidMount() {
+    console.log('CadastroScreen: componentDidMount');
+  }
+
+  componentWillUnmount() {
+    console.log('CadastroScreen: componentWillUnmount');
+  }
+
+  imprimirDados = () => {
+    const { nome, endereco, cidade, cep, telefone } = this.state;
+    const { username, password } = this.props;
+    Alert.alert(
+      'Dados do Cadastro',
+      `Usuário: ${username} \nSenha: ${password} \nNome: ${nome} \nEndereço: ${endereco} \nCidade: ${cidade} \nCEP: ${cep} \nTelefone: ${telefone}`,
+      [{ text: 'OK' }]
+    );
+  };
+
+  render() {
+    console.log('CadastroScreen: render');
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Cadastro</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome"
+          onChangeText={(nome) => this.setState({ nome })}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Endereço"
+          onChangeText={(endereco) => this.setState({ endereco })}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Cidade"
+          onChangeText={(cidade) => this.setState({ cidade })}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="CEP"
+          onChangeText={(cep) => this.setState({ cep })}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Telefone"
+          onChangeText={(telefone) => this.setState({ telefone })}
+        />
+        <View style={styles.buttonContainer}>
+          <Button title="Imprimir" onPress={this.imprimirDados} />
+          <Button title="Voltar" onPress={this.props.onVoltar} />
+          <Button
+            title="Sair"
+            onPress={() => {
+              if (Platform.OS === 'android') {
+                BackHandler.exitApp();
+              } else {
+                Alert.alert('Sair', 'Para sair, feche o app manualmente.');
+              }
+            }}
+          />
+        </View>
+      </View>
+    );
+  }
+}
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { loggedIn: false, username: '', password: '' };
+    console.log('App: constructor');
+  }
+
+  componentDidMount() {
+    console.log('App: componentDidMount');
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('App: componentDidUpdate', prevState, this.state);
+  }
+
+  componentWillUnmount() {
+    console.log('App: componentWillUnmount');
+  }
+
+  handleLogin = (username, password) => {
+    this.setState({ loggedIn: true, username, password });
+  };
+
+  handleVoltar = () => {
+    this.setState({ loggedIn: false, username: '', password: '' });
+  };
+
+  render() {
+    console.log('App: render');
+    return (
+      <View style={styles.container}>
+        {this.state.loggedIn ? (
+          <CadastroScreen
+            onVoltar={this.handleVoltar}
+            username={this.state.username}
+            password={this.state.password}
+          />
+        ) : (
+          <LoginScreen onLogin={this.handleLogin} />
+        )}
       </View>
     );
   }
@@ -63,15 +170,25 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f0f0f0',
     padding: 20,
   },
-  text: {
-    fontSize: 18,
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
     marginBottom: 10,
-    color: '#333',
+    padding: 10,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginTop: 20,
   },
 });
 
-export default MeuComponente;
