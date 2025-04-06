da1f1625725daf7fc018cd1072217b869c09795e
fernandaguzzo <guzzofernanda84@gmail.com>
2025-04-05 21:19:12 -0300

    continuação do terceiro exercício

--- navegacao/Ex03_NavHeader/App.js
+++ navegacao/Ex03_NavHeader/App.js
@@ -1,6 +1,6 @@
 import { Button } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from '@react-navigation/native-stack'; // Adicionei essa importação que estava faltando
+import { createNativeStackNavigator } from '@react-navigation/native-stack'; 
 import Home from "./Home";
 import Details from "./Details";
 
